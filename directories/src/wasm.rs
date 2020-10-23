cae39b415ac31b8b4593d31cb0763fcb5cf65769
Lzu Tao <taolzu@gmail.com>
2020-10-23 09:26:04 +0700

    rustfmt all the things
    It turns out that importing modules in cfg_if! macro makes `cargo fmt`
    ignore them.

--- directories/src/wasm.rs
+++ directories/src/wasm.rs
@@ -3,10 +3,18 @@
 use std::path::PathBuf;
 
 use crate::BaseDirs;
-use crate::UserDirs;
 use crate::ProjectDirs;
+use crate::UserDirs;
 
-pub fn base_dirs() -> Option<BaseDirs> { None }
-pub fn user_dirs() -> Option<UserDirs> { None }
-pub fn project_dirs_from_path(project_path: PathBuf) -> Option<ProjectDirs> { None }
-pub fn project_dirs_from(qualifier: &str, organization: &str, application: &str) -> Option<ProjectDirs> { None }
+pub fn base_dirs() -> Option<BaseDirs> {
+    None
+}
+pub fn user_dirs() -> Option<UserDirs> {
+    None
+}
+pub fn project_dirs_from_path(project_path: PathBuf) -> Option<ProjectDirs> {
+    None
+}
+pub fn project_dirs_from(qualifier: &str, organization: &str, application: &str) -> Option<ProjectDirs> {
+    None
+}
